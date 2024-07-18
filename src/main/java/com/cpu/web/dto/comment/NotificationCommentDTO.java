c85cb9668956479c10b502db3e8b4d666f01a067
slyhyun <lipsul061722@jbnu.ac.kr>
2024-07-18 18:50:48 +0900

    1.0.14

--- src/main/java/com/cpu/web/dto/comment/NotificationCommentDTO.java
+++ src/main/java/com/cpu/web/dto/comment/NotificationCommentDTO.java
@@ -1,7 +1,6 @@
 package com.cpu.web.dto.comment;
 
-import com.cpu.web.entity.board.Bulletin;
-import com.cpu.web.entity.comment.BulletinComment;
+
 import lombok.*;
 import java.time.LocalDateTime;
 
@@ -19,20 +18,4 @@ public class NotificationCommentDTO {
     private LocalDateTime postDate;
     private int notificationId;
 
-    @Getter
-    @Setter
-    public static class BulletinCommentDTO {
-
-        private Long bulletinId;
-        private String content;
-
-        public BulletinComment toBulletinCommentEntity(Bulletin bulletin){
-            BulletinComment bulletinComment = new BulletinComment();
-
-            bulletinComment.setContents(content);
-            bulletinComment.setBulletin(bulletin);
-
-            return bulletinComment;
-        }
-    }
 }
