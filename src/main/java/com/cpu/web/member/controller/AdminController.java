19180087e2ac600c415b69d5f7970b1a7e78396d
ParkDoHyeon <ehgus8910@gmail.com>
2024-09-12 20:27:56 +0900

    swagger admin 문서 수정

--- src/main/java/com/cpu/web/member/controller/AdminController.java
+++ src/main/java/com/cpu/web/member/controller/AdminController.java
@@ -3,6 +3,9 @@ package com.cpu.web.member.controller;
 import com.cpu.web.member.dto.CustomMember;
 import com.cpu.web.member.dto.MemberDTO;
 import com.cpu.web.member.service.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -26,12 +29,16 @@ public class AdminController {
 
     // 전체 유저 조회
     @GetMapping("/user")
+    @Operation(summary = "전체 유저 조회", description = "전체 유저 조회 API")
+    @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
     public List<MemberDTO> getAllUsers() {
         return adminService.getAllUser();
     }
 
     // 유저 권한 변경
     @PutMapping("/user/{id}")
+    @Operation(summary = "전체 유저 조회", description = "전체 유저 조회 API")
+    @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
     public ResponseEntity<MemberDTO> updateRole(@PathVariable Long id, @RequestParam String role) {
         if(role.equals("ROLE_ADMIN")||role.equals("ROLE_USER")){
             MemberDTO updateMemberDTO = adminService.updateRole(id, role);
@@ -42,6 +49,8 @@ public class AdminController {
 
     // 유저 삭제
     @DeleteMapping("/user/{id}")
+    @Operation(summary = "유저 삭제", description = "유저 삭제 API")
+    @ApiResponse(responseCode = "204", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         adminService.deleteUser(id);
         return ResponseEntity.noContent().build();
