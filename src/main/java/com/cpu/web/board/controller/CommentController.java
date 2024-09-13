6dccf9a324da2f08cee4970d1258808d6e26fbdc
ParkDoHyeon <ehgus8910@gmail.com>
2024-09-13 18:02:47 +0900

    게시판 Swagger 수정

--- src/main/java/com/cpu/web/board/controller/CommentController.java
+++ src/main/java/com/cpu/web/board/controller/CommentController.java
@@ -3,6 +3,13 @@ package com.cpu.web.board.controller;
 import com.cpu.web.board.dto.CommentDTO;
 import com.cpu.web.board.entity.Comment;
 import com.cpu.web.board.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +27,14 @@ public class CommentController {
 
     // 댓글 작성
     @PostMapping()
+    @Operation(summary = "댓글 작성", description = "댓글 작성 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "게시글 아이디", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "Long", example = "123"))),
+            @Parameter(name = "comment", description = "댓글 내용", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", example = "퍼가용~"))),
+    })
     public ResponseEntity<CommentDTO> createComment(@RequestBody CommentDTO commentDTO){
         Comment comment = commentService.createComment(commentDTO);
         URI location = ServletUriComponentsBuilder
@@ -32,12 +47,30 @@ public class CommentController {
     
     // 특정 글의 모든 댓글 조회
     @GetMapping("/{id}")
+    @Operation(summary = "특정 글의 모든 댓글 조회", description = "특정 글 모든 댓글 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 게시글입니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "게시글 아이디", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "Long", example = "123"))),
+            @Parameter(name = "comment", description = "댓글 내용", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", example = "퍼가용~"))),
+    })
     public List<CommentDTO> getAllComments(@PathVariable Long id) {
         return commentService.getAllComments(id);
     }
 
     // 댓글 수정
     @PutMapping("/{id}")
+    @Operation(summary = "댓글 수정", description = "댓글 수정 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 댓글입니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "게시글 아이디", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "Long", example = "123"))),
+            @Parameter(name = "comment", description = "댓글 내용", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", example = "퍼가용~"))),
+    })
     public ResponseEntity<CommentDTO> updateBulletinComment(@PathVariable Long id, @RequestBody CommentDTO commentDTO) {
         CommentDTO updatedComment = commentService.updateComment(id, commentDTO);
         return ResponseEntity.ok(updatedComment);
@@ -45,6 +78,10 @@ public class CommentController {
 
     // 댓글 삭제
     @DeleteMapping("/{id}")
+    @Operation(summary = "댓글 삭제", description = "댓글 삭제 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
+    })
     public ResponseEntity<?> deleteBulletinComment(@PathVariable Long id) {
         commentService.deleteComment(id);
         return ResponseEntity.noContent().build();  // 204 No Content
--- src/main/java/com/cpu/web/board/controller/PostController.java
+++ src/main/java/com/cpu/web/board/controller/PostController.java
@@ -5,6 +5,14 @@ package com.cpu.web.board.controller;
 import com.cpu.web.board.dto.PostDTO;
 import com.cpu.web.board.entity.Post;
 import com.cpu.web.board.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +25,21 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/post")
+@Tag(name = "Board", description = "게시판 API")
 public class PostController {
 
     private final PostService postService;
     //글 작성
     @PostMapping
+    @Operation(summary = "게시글 작성", description = "게시글 작성 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Parameters({
+            @Parameter(name = "isNotice", description = "공지 여부", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "boolean", example = "true"))),
+            @Parameter(name = "title", description = "제목", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", example = "박관소 개최 안내"))),
+            @Parameter(name = "content", description = "내용", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", example = "2024년 하반기 박관소가 개최 예정입니다.")))
+    })
     public ResponseEntity<PostDTO> createBulletin(@RequestBody PostDTO postDTO) {
         Post post = postService.createPost(postDTO);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -33,6 +51,10 @@ public class PostController {
 
     // 페이징 처리된 전체 글 조회
     @GetMapping
+    @Operation(summary = "게시글 전체 조회", description = "게시글 전체 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
+    })
     public Page<PostDTO> getAllBulletins(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
@@ -41,6 +63,11 @@ public class PostController {
 
     // 특정 글 조회
     @GetMapping("/{id}")
+    @Operation(summary = "게시글 개별 조회", description = "게시글 개별 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 게시글입니다..", content = @Content(mediaType = "application/json"))
+    })
     public ResponseEntity<PostDTO> getBulletinById(@PathVariable Long id) {
         Optional<PostDTO> bulletinDTO = postService.getPostById(id);
         return bulletinDTO.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
@@ -48,6 +75,14 @@ public class PostController {
 
     // 글 수정
     @PutMapping("/{id}")
+    @Operation(summary = "게시글 수성", description = "게시글 수정 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Parameters({
+            @Parameter(name = "title", description = "제목", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", example = "박관소 개최 안내"))),
+            @Parameter(name = "email", description = "이메일", content = @Content(mediaType = "multipart/form-data", schema = @Schema(type = "string", example = "2024년 하반기 박관소가 개최 예정입니다.")))
+    })
     public  ResponseEntity<PostDTO> updateBulletin(@PathVariable Long id, @RequestBody PostDTO postDTO) {
         PostDTO updatedPostDTO = postService.updatePost(id, postDTO);
         return ResponseEntity.ok(updatedPostDTO);
@@ -55,6 +90,10 @@ public class PostController {
 
     // 글 삭제
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제", description = "게시글 삭제 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
+    })
     public ResponseEntity<?> deleteBulletin(@PathVariable Long id) {
         postService.deletePost(id);
         return ResponseEntity.noContent().build();
