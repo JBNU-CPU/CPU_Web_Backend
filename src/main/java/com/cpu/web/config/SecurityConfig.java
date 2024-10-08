cabb6748d0d56406e7a868f4235a2ca3f95df96e
ParkDoHyeon <ehgus8910@jbnu.ac.kr>
2024-10-08 21:34:22 +0900

    cors에러 해결

--- src/main/java/com/cpu/web/config/SecurityConfig.java
+++ src/main/java/com/cpu/web/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.cpu.web.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -21,7 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf((csrf) -> csrf.disable());
-        http.cors(cors -> cors.disable());
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource));
 
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/", "/login", "/loginProc", "/signup", "/signupProc", "/auth/**").permitAll()  // 이메일 인증 경로 허용
