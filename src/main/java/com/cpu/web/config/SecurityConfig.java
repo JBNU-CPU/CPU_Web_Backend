cd99e9378f4fd3f783fdcff77ab7893ada8607fd
ParkDoHyeon <ehgus8910@jbnu.ac.kr>
2024-11-12 22:06:37 +0900

    테스트

--- src/main/java/com/cpu/web/config/SecurityConfig.java
+++ src/main/java/com/cpu/web/config/SecurityConfig.java
@@ -29,11 +29,11 @@ public class SecurityConfig {
         http.cors(cors -> cors.configurationSource(corsConfigurationSource));
 
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/", "/login", "/loginProc", "/signup", "/signupProc", "/auth/**").permitAll()  // 이메일 인증 경로 허용
+                .requestMatchers("/", "/login", "/loginProc", "/signup", "/signupProc", "/auth/**", "/swagger-ui/index.html").permitAll()  // 이메일 인증 경로 허용
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/study/**").permitAll()
                 .requestMatchers("/post/**").permitAll() //test용 0915준혁
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
         );
 
         http.formLogin((formLogin) -> formLogin
