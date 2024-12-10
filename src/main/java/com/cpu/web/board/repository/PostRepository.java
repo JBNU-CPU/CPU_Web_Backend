1f5fe0650f5f6d4bfa7f0be2110c5e0778b84ff5
dohyeon <ehgus8910@jbnu.ac.kr>
2024-12-10 17:51:20 +0900

    팀장 여부 필드 추가

--- src/main/java/com/cpu/web/board/repository/PostRepository.java
+++ src/main/java/com/cpu/web/board/repository/PostRepository.java
@@ -4,7 +4,6 @@ import com.cpu.web.board.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
--- src/main/java/com/cpu/web/scholarship/entity/Member_Study.java
+++ src/main/java/com/cpu/web/scholarship/entity/Member_Study.java
@@ -21,6 +21,9 @@ public class Member_Study {
     @CreationTimestamp
     private Timestamp createDate;
 
+    @Column(name = "is_leader", nullable = false) // 팀장 여부
+    private Boolean isLeader;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
