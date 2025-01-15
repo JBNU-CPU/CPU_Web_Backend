af44a474439ef44245bb07ae7c64b2e77c7b31cc
dohyeon <ehgus8910@jbnu.ac.kr>
2025-01-15 22:03:02 +0900

    cors 수정

--- src/main/java/com/cpu/web/config/CorsConfig.java
+++ src/main/java/com/cpu/web/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("https://jbnu-cpu.github.io")); // 모든 출처 허용
+        configuration.setAllowedOrigins(List.of("http://13.209.68.235")); // 모든 출처 허용
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // 모든 출처 허용 시 credentials를 false로 설정해야 함
