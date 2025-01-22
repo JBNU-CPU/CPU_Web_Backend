381d13638594ab9f37d55e5045694baf3b206e4e
ParkDoHyeon <ehgus8910@jbnu.ac.kr>
2025-01-22 20:15:30 +0900

    기본회원가입 멤버로 변경

--- src/main/java/com/cpu/web/member/service/SignupService.java
+++ src/main/java/com/cpu/web/member/service/SignupService.java
@@ -41,7 +41,7 @@ public class SignupService {
         member.setEmail(signupDTO.getEmail());
 
         // 기본적으로 ROLE_GUEST 설정
-        member.setRole(Role.ROLE_ADMIN);
+        member.setRole(Role.ROLE_MEMBER);
 
         // 회원 정보 저장
         member = memberRepository.save(member);
