c0c4d9baddc510a915d4cac399ead35fb56c3aa1
slyhyun <lipsul061722@jbnu.ac.kr>
2025-01-22 23:33:08 +0900

    Study Func

--- src/main/java/com/cpu/web/scholarship/repository/StudyRepository.java
+++ src/main/java/com/cpu/web/scholarship/repository/StudyRepository.java
@@ -1,7 +1,10 @@
 package com.cpu.web.scholarship.repository;
 
 import com.cpu.web.scholarship.entity.Study;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyRepository extends JpaRepository<Study,Long> {
+    Page<Study> findAll(Pageable pageable);
 }
