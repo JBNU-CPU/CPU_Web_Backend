9b027b0e638d2891741b072a1afba30054cd893e
ParkDoHyeon <ehgus8910@jbnu.ac.kr>
2025-02-05 19:33:03 +0900

    회원가입 중복 검사 응답 메시지 수정

--- src/main/java/com/cpu/web/member/service/SignupService.java
+++ src/main/java/com/cpu/web/member/service/SignupService.java
@@ -3,6 +3,7 @@ package com.cpu.web.member.service;
 import com.cpu.web.member.dto.SignupDTO;
 import com.cpu.web.member.entity.Member;
 import com.cpu.web.member.entity.Member.Role;
+import com.cpu.web.member.exception.DuplicateResourceException;
 import com.cpu.web.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,17 +20,17 @@ public class SignupService {
 
         // ID 중복 여부 체크
         if (memberRepository.existsByUsername(signupDTO.getUsername())) {
-            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+            throw new DuplicateResourceException("이미 존재하는 아이디입니다.");
         }
 
         // 닉네임 중복 여부 체크
         if (memberRepository.existsByNickName(signupDTO.getNickName())) {
-            throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
+            throw new DuplicateResourceException("이미 존재하는 닉네임입니다.");
         }
 
         // 이메일 중복 여부 체크
         if (memberRepository.existsByEmail(signupDTO.getEmail())) {
-            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+            throw new DuplicateResourceException("이미 존재하는 이메일입니다.");
         }
 
         // 중복 검사를 통과하면 회원가입 진행
