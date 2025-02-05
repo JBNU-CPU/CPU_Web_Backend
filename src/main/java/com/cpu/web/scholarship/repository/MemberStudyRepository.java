d38c7f71408b1a73b96ed5e1169f28257737528c
slyhyun <lipsul061722@jbnu.ac.kr>
2025-02-05 22:19:17 +0900

    Study Apply

--- src/main/java/com/cpu/web/scholarship/repository/MemberStudyRepository.java
+++ src/main/java/com/cpu/web/scholarship/repository/MemberStudyRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberStudyRepository extends JpaRepository<MemberStudy, Long> {
-    // 특정 멤버가 특정 스터디에 참여하고 있는지 확인하는 메소드
-    boolean existsByStudy_StudyIdAndMember_MemberIdAndIsLeader(Long studyId, Long memberId, boolean isLeader);  // 여기에 추가
-}
+    // 특정 멤버가 특정 스터디에 참여하고 있는지 확인
+    boolean existsByStudy_StudyIdAndMember_MemberId(Long studyId, Long memberId);
 
+    // 특정 멤버가 리더인지 확인
+    boolean existsByStudy_StudyIdAndMember_MemberIdAndIsLeader(Long studyId, Long memberId, boolean isLeader);
+
+    // 특정 멤버의 신청 정보 조회
+    Optional<MemberStudy> findByStudy_StudyIdAndMember_MemberId(Long studyId, Long memberId);
+}
