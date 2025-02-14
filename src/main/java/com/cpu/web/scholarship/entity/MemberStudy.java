0c11bac757ae75206cde2c77846c9335f8eecaa6
ParkDoHyeon <ehgus8910@jbnu.ac.kr>
2025-02-14 22:37:48 +0900

    에러 확인

--- src/main/java/com/cpu/web/scholarship/entity/MemberStudy.java
+++ src/main/java/com/cpu/web/scholarship/entity/MemberStudy.java
@@ -1,35 +1,35 @@
-//package com.cpu.web.scholarship.entity;
-//
-//import com.cpu.web.member.entity.Member;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.hibernate.annotations.CreationTimestamp;
-//
-//import java.sql.Timestamp;
-//
-//@Entity
-//@Getter
-//@Setter
-//public class MemberStudy {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "member_study_id")
-//    private Long memberStudyId;
-//
-//    @CreationTimestamp
-//    private Timestamp joinDate;
-//
-//    @Column(name = "is_leader", nullable = false) // 팀장 여부
-//    private Boolean isLeader;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id", nullable = false)
-//    private Member member;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "study_id", nullable = false)
-//    private Study study;
-//
-//}
+package com.cpu.web.scholarship.entity;
+
+import com.cpu.web.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+public class MemberStudy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_study_id")
+    private Long memberStudyId;
+
+    @CreationTimestamp
+    private Timestamp joinDate;
+
+    @Column(name = "is_leader", nullable = false) // 팀장 여부
+    private Boolean isLeader;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", nullable = false)
+    private Study study;
+
+}
