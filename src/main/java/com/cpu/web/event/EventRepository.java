3affff88f801bc35388ab1d4f61ac517802bd94c
ParkDoHyeon <ehgus8910@jbnu.ac.kr>
2025-02-24 19:49:45 +0900

    이벤트 스코어 등록 로직 수정

--- src/main/java/com/cpu/web/event/EventRepository.java
+++ src/main/java/com/cpu/web/event/EventRepository.java
@@ -3,4 +3,7 @@ package com.cpu.web.event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    boolean existsByUserId(String userId);
+
+    Event findByUserId(String userId);
 }
--- src/main/java/com/cpu/web/event/EventService.java
+++ src/main/java/com/cpu/web/event/EventService.java
@@ -8,7 +8,19 @@ import org.springframework.stereotype.Service;
 public class EventService {
 
     private final EventRepository eventRepository;
+    
     public Event createScore(EventRequestDTO eventRequestDTO) {
-        return eventRepository.save(eventRequestDTO.toEventEntity());
+
+        if (eventRepository.existsByUserId(eventRequestDTO.getUserId())){ // 이미 등록한 유저 점수 더 높을 경우 갱신
+            Event event = eventRepository.findByUserId(eventRequestDTO.getUserId());
+            if(eventRequestDTO.getScore() > event.getScore()){
+                event.setScore(eventRequestDTO.getScore());
+                return eventRepository.save(eventRequestDTO.toEventEntity());
+            }else{
+                return event;
+            }
+        }else{ // 신규 등록
+            return eventRepository.save(eventRequestDTO.toEventEntity());
+        }
     }
 }
