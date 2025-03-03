f7e453b37f0c81786e307b434c0776cf78e42897
slyhyun <lipsul061722@jbnu.ac.kr>
2025-03-03 19:15:01 +0900

    에러 수정

--- src/main/java/com/cpu/web/member/controller/PasswordResetController.java
+++ src/main/java/com/cpu/web/member/controller/PasswordResetController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/password")
 public class PasswordResetController {
 
     @Autowired
