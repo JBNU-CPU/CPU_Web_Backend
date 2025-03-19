d8f9bd457d1f1c0759ed7c354fd0df52e08e5e69
slyhyun <lipsul061722@jbnu.ac.kr>
2025-03-20 00:48:38 +0900

    에러 수정

--- src/main/java/com/cpu/web/scholarship/dto/request/ApplyRequestDTO.java
+++ src/main/java/com/cpu/web/scholarship/dto/request/ApplyRequestDTO.java
@@ -1,13 +1,4 @@
 package com.cpu.web.scholarship.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ApplyRequestDTO {
 }
