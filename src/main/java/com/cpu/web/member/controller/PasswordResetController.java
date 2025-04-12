ac9e047ade3fc16f0034e402be71e2d5df0f7a04
dohyeon <ehgus8910@gmail.com>
2025-04-13 01:12:01 +0900

    비밀번호 재설정 오류 픽스

--- src/main/java/com/cpu/web/member/controller/PasswordResetController.java
+++ src/main/java/com/cpu/web/member/controller/PasswordResetController.java
@@ -36,7 +36,7 @@ public class PasswordResetController {
             @ApiResponse(responseCode = "200", description = "비밀번호가 성공적으로 재설정되었습니다.", content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "400", description = "이메일 인증되지 않았거나 사용자를 찾을 수 없습니다.", content = @Content(mediaType = "application/json"))
     })
-    public ResponseEntity<String> resetPassword(@RequestParam String email, @RequestParam String newPassword, HttpSession session) {
+    public ResponseEntity<String> resetPassword(@RequestPart String email, @RequestPart String newPassword, HttpSession session) {
         return passwordResetService.resetPassword(email, newPassword)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.badRequest().body("비밀번호 재설정 중 오류가 발생했습니다."));
